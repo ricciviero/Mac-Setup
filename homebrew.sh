1fda97fe7c1d5aa31486dc9538370b65c18f7afb
Riccardo Civiero <riccardociviero98@icloud.com>
2024-11-13 12:09:00 +0100

    casks updated

--- homebrew.sh
+++ homebrew.sh
@@ -6,13 +6,29 @@
 # Packages:
 brew install git node python wget speedtest-cli ffmpeg
 
-# Applications:
-brew install --cask warp firefox-developer-edition discord obsidian figma github visual-studio-code chatgpt
-brew install --cask notion handbrake google-chrome copyclip nordvpn logi-options+ raycast vlc telegram whatsapp messenger
-brew install --cask the-unarchiver transmission zoom docker alt-tab stats slack
+# Applications by Category:
 
+# Dev
+brew install --cask warp visual-studio-code cursor github-copilot-for-xcode figma
+
+# Web Browsing
+brew install --cask firefox@developer-edition google-chrome
+
+# Communication
+brew install --cask discord telegram whatsapp messenger slack zoom
+
+# Productivity
+brew install --cask notion chatgpt raycast cap command-x
+
+# Security and System Utilities
+brew install --cask nordvpn hiddenbar stats alt-tab logi-options+
+
+# Multimedia
+brew install --cask vlc handbrake 4k-youtube-to-mp3 spotify
+
+# Storage and Compression Tools
+brew install --cask the-unarchiver transmission
 
 echo "Setup complete."
 
-# Runt the script in terminal:
-sh homebrew.sh
+# Run the script in terminal: sh homebrew.sh
